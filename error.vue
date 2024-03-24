f96de56d1f85abe91cbb80dc37c28aac6f5d401a
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-03-24 23:23:15 +0300

    page error

--- error.vue
+++ error.vue
@@ -1,14 +1,3 @@
-<template>
-  <main class="flex h-screen w-screen flex-col items-center justify-center">
-    <div class="text-center">
-      <p class="text-3xl font-700">{{ $props.error.statusCode }}</p>
-      <p class="text-gray-600 mb-5 mt-5 text-base leading-7">Error: {{ $props.error.url }}</p>
-      <p class="text-gray-600 mb-5 mt-5 text-base leading-7">{{ $props.error.message }}</p>
-      <button type="button" @click="handleError"> Home </button>
-    </div>
-  </main>
-</template>
-
 <script setup>
   const props = defineProps({
     error: Object,
@@ -16,3 +5,74 @@
 
   const handleError = () => clearError({ redirect: "/" });
 </script>
+
+<template>
+  <main class="container">
+    <div class="block">
+      <p class="title">
+        Ошибка: 
+          <span>
+            {{ $props.error.statusCode }}
+          </span>
+      </p>
+      <p class="text">{{ $props.error.message }}</p>
+      <button type="button" @click="handleError"> Вернуться на главную </button>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.container{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .block{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .title{
+      font-size: 36px;
+      margin-bottom: 10px;
+
+      span{
+        font-weight: 700;
+        color: var(--blue);
+      }
+    }
+    .text{
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
+  }
+  button{
+      font-size: 18px;
+      font-weight: 700;
+      color: var(--white);
+      background-color: var(--blue);
+      border: 1px solid var(--blue);
+      padding: 9px 18px;
+      margin-bottom: 20px;
+      transition: all 0.25s ease;
+
+      @media (hover: hover) {
+          &:hover{
+              color: var(--blue);
+              background-color: var(--white);
+              border: 1px solid var(--blue);
+          }
+      }
+
+      &:active{
+          color: var(--white);
+          background-color: var(--blue);
+          border: 1px solid var(--blue);
+      }
+      
+  }
+}
+</style>
