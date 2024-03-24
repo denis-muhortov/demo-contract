1fe411187a537a0f4cac1c5d7fe88cd3e86009cd
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-03-25 00:19:21 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,13 +15,6 @@ export default defineNuxtConfig({
         // { src: 'https://awesome-lib.js' }
       ],
       link: [
-        {
-          rel: "preload", 
-          fetchpriority: "high",
-          as: "image",
-          href: "https://tbhhbwhszmtjaznapwbc.supabase.co/storage/v1/object/public/demo/main_mobile_screen.webp",
-          type: "image/webp"
-        },
         // <link rel="stylesheet" href="https://myawesome-lib.css">
         // { rel: "stylesheet", href: "https://awesome-lib.css" },
       ],
