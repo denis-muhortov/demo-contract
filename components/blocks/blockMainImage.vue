71c47a05be7530c24285984c4f0f7b0a0e4bcbc0
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-03-25 07:55:13 +0300

    resize

--- components/blocks/blockMainImage.vue
+++ components/blocks/blockMainImage.vue
@@ -8,3 +8,12 @@
         class="image"
     />
 </template>
+<style lang="scss" scoped>
+@media screen and (max-width: 1800px) { .image{height: 780px !important;}}
+@media screen and (max-width: 1440px) { .image{height: 780px !important;}}
+@media screen and (max-width: 1240px) { .image{height: 710px !important;}}
+@media screen and (max-width: 1020px) { .image{height: 610px !important;}}
+@media screen and (max-width: 800px) { .image{height: 610px !important;}}
+@media screen and (max-width: 640px) { .image{height: 720px !important;}}
+@media screen and (max-width: 380px) { .image{height: 1070px !important;}}
+</style>
