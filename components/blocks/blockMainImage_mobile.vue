b3110034ae5fa10f3422f23d1479c3f36847d6c9
Ademaru <58119776+denis-muhortov@users.noreply.github.com>
2024-03-25 07:01:07 +0300

    test

--- components/blocks/blockMainImage_mobile.vue
+++ components/blocks/blockMainImage_mobile.vue
@@ -1,9 +1,16 @@
 <template>
     <UnLazyImage
-        src="https://tbhhbwhszmtjaznapwbc.supabase.co/storage/v1/object/public/demo/main_mobile_screen.webp"
+        src="https://tbhhbwhszmtjaznapwbc.supabase.co/storage/v1/object/public/demo/main_photo_mini__mobile_2.webp"
         blurhash="CVIM=~57S}r?}[WCX8e:"
         auto-sizes
         alt="Главное фото"
         class="image"
     />
 </template>
+<style lang="scss" scoped>
+@media screen and (max-width: 1020px) {
+    .image{
+        max-height: 610px;
+    }
+}
+</style>
